9adb602b7ec51467281ede671a0f816e240131c8
Aad Versteden <aad@knowified.com>
2014-05-22 13:55:36 +0200

    making the shell script sh compliant

--- scrape-eba.sh
+++ scrape-eba.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+SPIDERS="ebaTable ebaStress ebaExercise"
+
 rm -R out; mkdir out
-for spiderName in {ebaTable,ebaStress,ebaExercise}
+
+for spiderName in $SPIDERS
 do
     scrapy crawl $spiderName
 done
+
 rm datasets.zip
 zip -r datasets.zip out
